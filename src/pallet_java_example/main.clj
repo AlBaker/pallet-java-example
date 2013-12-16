c49075804414d521c90633e3353d323d511631c3
AlBaker <albaker@clarkparsia.com>
2013-12-15 23:02:17 -0500

    Converge example

--- src/pallet_java_example/main.clj
+++ src/pallet_java_example/main.clj
@@ -12,7 +12,17 @@
             [pallet.phase :as pphase]
             [pallet.actions :as pact]
             [clojure.java.io :as io]
+            [pallet-java-example.groups.pallet-java-example :as pvm]
             [clojure.java.shell :as csh]))
 
 
+;; Simple example showing creatin of a compute service provider, via vmfest
+;; and the converge of it
+;; Note you'll have to shut down the VM manually in this example
+(defn -main [& args]
+  (do
+    (pvm/init-vmfest)
+    ((converge {pvm/example-group 1} :compute (:vmfest @pvm/provider))
+      (System/exit 0))
+    ))
 
